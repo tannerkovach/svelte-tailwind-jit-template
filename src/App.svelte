2791b853a6b430acaca672bdc235656b270f2f4f
tannerkovach <kovach.tanner@gmail.com>
2021-08-22 00:55:38 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Counter from './components/Counter.svelte';
+</script>
+
+<main>
+	<h1 class="bg-blue-100 mb-4">Hello</h1>
+	<p class="mb-[100px]">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Counter />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,8 @@
+<script>
+    let count = 0;
+    let plusOne = function() {
+        count++;
+    }
+</script>
+
+<button class="border border-gray-500 rounded-md hover:bg-gray-300 px-4 py-1" on:click="{plusOne}">Count - {count} </button>
